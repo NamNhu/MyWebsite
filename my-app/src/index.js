3dff83bae4007ca8010255bd845ede6104d3fe5f
Nam Dan Nhu <namdannhu@MBP-von-Nam.fritz.box>
2023-03-16 18:34:55 +0100

    react tutorial

---
+++ my-app/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>
+    }
+}
+
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    } 
+
+
+    render() {
+        return (
+            <button 
+            className='square' 
+            onClick={ () => this.setState({ value: 'X'})}
+            >
+                {this.state.value}
+            </button>
+        );
+    } 
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Square/>)
